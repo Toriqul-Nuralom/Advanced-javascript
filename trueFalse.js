2ae142f9e226ed784fe7c7fdc09083380132d27b
toriqul isalm <toriqul.nur.islam@gmai.com>
2021-02-04 01:05:21 +0600

    undefined vs null done for interview

---
+++ trueFalse.js
@@ -0,0 +1,16 @@
+// falsey
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truethy
+// 0, [], {}
+let name = "false";
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
---
+++ undefined-vs-null.js
@@ -0,0 +1,24 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+const result = add(5,15);
+console.log(result);
+
+function add1(num1, num2){
+    console.log(num2);
+}
+const result1 = add1(5);
+console.log(result1);
+
+const premik = {name:"smart dude", phone:74865};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let age = [9, 15, 12]
+console.log(age[11]);
