c738534831f297b6d5e47fd35b2821fe19343370
toriqul isalm <toriqul.nur.islam@gmai.com>
2021-02-04 17:08:09 +0600

    map filter find all most done

--- map-filter-find.js
+++ map-filter-find.js
@@ -1,20 +1,20 @@
 const numbers = [3, 4, 5, 6, 7, 8];
-// const outPut = [];
-// for(let i = 0; i < numbers.length; i++){
-//     const element = numbers[i];
-//     const result = element * element;
-//     outPut.push(result);
-// }
+const outPut = [];
+for(let i = 0; i < numbers.length; i++){
+    const element = numbers[i];
+    const result = element * element;
+    outPut.push(result);
+}
 
-// function square(element){
-//     return element * element;
-// }
+function square(element){
+    return element * element;
+}
 
-// const square = element => element * element;
-// const square = x => x * x;
+const square = element => element * element;
+const square = x => x * x;
 
-// const result = numbers.map(x => x * x)
-// console.log(result);
+const result = numbers.map(x => x * x)
+console.log(result);
 
 
 // Filter
