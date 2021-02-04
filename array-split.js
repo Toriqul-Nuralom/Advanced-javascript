bc141f1f465ee95788bac5cb2c9955a6ca4559c7
toriqul isalm <toriqul.nur.islam@gmai.com>
2021-02-05 04:00:36 +0600

    some thing is complitet

---
+++ array-split.js
@@ -0,0 +1,14 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// const part = nums.slice(2,5);
+const part2 = nums.slice(2,6)
+console.log(part2);
+
+// const remove = nums.splice(2,3);
+// const remove2 = nums.splice(3,3, 77,82)
+// console.log(remove2);
+// console.log(nums);
+
+// const together = nums.join('ami ');
+// const together1 = nums.join(",");
+// console.log(together1);
+// console.log(nums);
